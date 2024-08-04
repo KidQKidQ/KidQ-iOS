125f82f07d3e9b92a032dc31dafaa6c718766541
Jaehwi Kim <jaehwi95@gmail.com>
2024-08-05 02:30:51 +0900

    feat/KQS-15: init project
    - init project

---
+++ kidq/kidq/kidqApp.swift
@@ -0,0 +1,17 @@
+//
+//  kidqApp.swift
+//  kidq
+//
+//  Created by Jaehwi Kim on 8/5/24.
+//
+
+import SwiftUI
+
+@main
+struct kidqApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
